ac7c5b1b19e2c28bb513f2eb031bd0f63a309a78
miltonminecraft-afk <milton.minecraft@gmail.com>
2026-08-31 12:56:57 +0200

    Delete tools/assistant directory

--- tools/assistant/runtime.js
+++
@@ -1,9 +0,0 @@
-import { AndroidNativeRuntime } from './runtimes/android-native.js';
-import { WebWllamaRuntime } from './runtimes/web-wllama.js';
-
-export function createRuntime(){
-  if (window.ToolsNativeLlama && typeof window.ToolsNativeLlama.getRuntimeInfo === 'function') {
-    return new AndroidNativeRuntime(window.ToolsNativeLlama);
-  }
-  return new WebWllamaRuntime();
-}
